2ff6b4925847fe29ba0a45acd413a2837bbbb992
sigmastar2017 <2079688829@qq.com>
2020-02-10 10:00:52 +0800

    add reflective loader

--- injectDll/injectDll.cpp
+++ injectDll/injectDll.cpp
@@ -2,5 +2,126 @@
 //
 
 #include "stdafx.h"
+#include "injectDll.h"
+#include <stdio.h>
+#define DLL_PATH "C:\\Test\\reflective_loader.dll"
+#define BREAK_WITH_ERROR(message) MessageBoxA(NULL,message,"Error",MB_OK);return 0;
 
+DWORD Rva2Offset(DWORD dwRva, UINT_PTR uiBaseAddress)
+{
+	WORD wIndex = 0;
+	PIMAGE_SECTION_HEADER pSectionHeader = NULL;
+	PIMAGE_NT_HEADERS pNtHeaders = NULL;
+	pNtHeaders = (PIMAGE_NT_HEADERS)(uiBaseAddress + ((PIMAGE_DOS_HEADER)uiBaseAddress)->e_lfanew);
+	pSectionHeader = (PIMAGE_SECTION_HEADER)((UINT_PTR)(&pNtHeaders->OptionalHeader) + pNtHeaders->FileHeader.SizeOfOptionalHeader);
+	if (dwRva < pSectionHeader[0].PointerToRawData)
+	{
+		return dwRva;
+	}
+	for (wIndex = 0; wIndex < pNtHeaders->FileHeader.NumberOfSections; wIndex++)
+	{
+		if (dwRva >= pSectionHeader[wIndex].VirtualAddress && dwRva < (pSectionHeader[wIndex].VirtualAddress + pSectionHeader[wIndex].SizeOfRawData))
+		{
+			return (dwRva - pSectionHeader[wIndex].VirtualAddress + pSectionHeader[wIndex].PointerToRawData);
+		}
+	}
+	return 0;
+}
 
+DWORD GetReflectiveLoaderOffset(VOID * lpReflectiveDllBuffer)
+{
+	UINT_PTR uiBaseAddress = 0;
+	UINT_PTR uiExportDir = 0;
+	UINT_PTR uiNameArray = 0;
+	UINT_PTR uiAddressArray = 0;
+	UINT_PTR uiNameOrdinals = 0;
+	DWORD dwCounter = 0;
+#ifdef WIN_X64
+	DWORD dwCompiledArch = 2;
+#else
+	DWORD dwCompiledArch = 1;
+#endif
+	uiBaseAddress = (UINT_PTR)lpReflectiveDllBuffer;
+	uiExportDir = uiBaseAddress + ((PIMAGE_DOS_HEADER)uiBaseAddress)->e_lfanew;
+	if (((PIMAGE_NT_HEADERS)uiExportDir)->OptionalHeader.Magic == 0x010B) // PE32
+	{
+		if (dwCompiledArch != 1)
+			return 0;
+	}
+	else if (((PIMAGE_NT_HEADERS)uiExportDir)->OptionalHeader.Magic == 0x020B) // PE64
+	{
+		if (dwCompiledArch != 2)
+			return 0;
+	}
+	else
+	{
+		return 0;
+	}
+	uiNameArray = (UINT_PTR)&((PIMAGE_NT_HEADERS)uiExportDir)->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+	uiExportDir = uiBaseAddress + Rva2Offset(((PIMAGE_DATA_DIRECTORY)uiNameArray)->VirtualAddress, uiBaseAddress);
+	uiNameArray = uiBaseAddress + Rva2Offset(((PIMAGE_EXPORT_DIRECTORY)uiExportDir)->AddressOfNames, uiBaseAddress);
+	uiAddressArray = uiBaseAddress + Rva2Offset(((PIMAGE_EXPORT_DIRECTORY)uiExportDir)->AddressOfFunctions, uiBaseAddress);
+	uiNameOrdinals = uiBaseAddress + Rva2Offset(((PIMAGE_EXPORT_DIRECTORY)uiExportDir)->AddressOfNameOrdinals, uiBaseAddress);
+	dwCounter = ((PIMAGE_EXPORT_DIRECTORY)uiExportDir)->NumberOfNames;
+	while (dwCounter--)
+	{
+		char * cpExportedFunctionName = (char *)(uiBaseAddress + Rva2Offset(DEREF_32(uiNameArray), uiBaseAddress));
+		if (strstr(cpExportedFunctionName, "ReflectiveLoader") != NULL)
+		{
+			uiAddressArray = uiBaseAddress + Rva2Offset(((PIMAGE_EXPORT_DIRECTORY)uiExportDir)->AddressOfFunctions, uiBaseAddress);
+			uiAddressArray += (DEREF_16(uiNameOrdinals) * sizeof(DWORD));
+			return Rva2Offset(DEREF_32(uiAddressArray), uiBaseAddress);
+		}
+		uiNameArray += sizeof(DWORD);
+		uiNameOrdinals += sizeof(WORD);
+	}
+	return 0;
+}
+
+HANDLE LoadRemoteLibrary(LPVOID lpLibraryBuffer, DWORD dwLength)
+{
+	DWORD dwReflectiveLoaderOffset = GetReflectiveLoaderOffset(lpLibraryBuffer);
+	if (!dwReflectiveLoaderOffset)
+	{
+		return INVALID_HANDLE_VALUE;
+	}
+	LPTHREAD_START_ROUTINE lpReflectiveLoader = (LPTHREAD_START_ROUTINE)((ULONG_PTR)lpLibraryBuffer + dwReflectiveLoaderOffset);
+	HANDLE hThread = INVALID_HANDLE_VALUE;
+	DWORD dwThreadId;
+	hThread = CreateRemoteThread(GetCurrentProcess(), NULL, 0, lpReflectiveLoader, NULL, NULL, &dwThreadId);
+	if (hThread == INVALID_HANDLE_VALUE)
+	{
+		MessageBoxA(NULL, "Failed to create Thread!", "Info", MB_OK);
+	}
+	return hThread;
+}
+
+DWORD WINAPI ReflectiveDllLoader(LPVOID lpParam)
+{
+	HANDLE hFile = CreateFileA(DLL_PATH, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		BREAK_WITH_ERROR("Failed to open the DLL file");
+	}
+	DWORD dwLength = GetFileSize(hFile, NULL);
+	if (dwLength == INVALID_FILE_SIZE || dwLength == 0)
+	{
+		BREAK_WITH_ERROR("Failed to get the DLL file size");
+	}
+	LPVOID lpBuffer = VirtualAlloc(NULL, dwLength, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (!lpBuffer)
+	{
+		BREAK_WITH_ERROR("Failed to alloc memory");
+	}
+	DWORD dwNumberBytesRead;
+	if (ReadFile(hFile, lpBuffer, dwLength, &dwNumberBytesRead, NULL) == FALSE)
+	{
+		BREAK_WITH_ERROR("Failed to read file");
+	}
+	HANDLE hThread = LoadRemoteLibrary(lpBuffer, dwLength);
+	if (hThread == INVALID_HANDLE_VALUE)
+	{
+		BREAK_WITH_ERROR("Failed to inject the DLL");
+	}
+	return 0;
+}
--- kernalApc/mydriver.c
+++ kernalApc/mydriver.c
@@ -2,11 +2,11 @@
 
 
 
-GET_ADDRESS Hash = {0};
+GET_ADDRESS Hash = { 0 };
 
 NTSTATUS DllInject(HANDLE ProcessId, PEPROCESS Peprocess, PETHREAD Pethread, BOOLEAN Alert) {
 
-	HANDLE hProcess; 
+	HANDLE hProcess;
 	OBJECT_ATTRIBUTES oa = { sizeof(OBJECT_ATTRIBUTES) };
 	CLIENT_ID cidprocess = { 0 };
 	CHAR DllFormatPath[] = DLLPATH;
@@ -21,21 +21,21 @@ NTSTATUS DllInject(HANDLE ProcessId, PEPROCESS Peprocess, PETHREAD Pethread, BOO
 
 		if (NT_SUCCESS(ZwAllocateVirtualMemory(hProcess, &pvMemory, 0, &Size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE))) {
 
-			    KAPC_STATE KasState;
-				PKAPC Apc;
-				KeStackAttachProcess(Peprocess, &KasState);
-				strcpy(pvMemory, DllFormatPath);
-				KeUnstackDetachProcess(&KasState);
+			KAPC_STATE KasState;
+			PKAPC Apc;
+			KeStackAttachProcess(Peprocess, &KasState);
+			strcpy(pvMemory, DllFormatPath);
+			KeUnstackDetachProcess(&KasState);
 
-				Apc = (PKAPC)ExAllocatePool(NonPagedPool, sizeof(KAPC));
-				if (Apc && Hash.pvLoadLibraryExA ){
-					DbgPrint("I am in dllinject:[%p],pvMemory:[%p]", Hash.pvLoadLibraryExA, pvMemory);
-					//DbgBreakPoint();
+			Apc = (PKAPC)ExAllocatePool(NonPagedPool, sizeof(KAPC));
+			if (Apc && Hash.pvLoadLibraryExA) {
+				DbgPrint("I am in dllinject:[%p],pvMemory:[%p]", Hash.pvLoadLibraryExA, pvMemory);
+				//DbgBreakPoint();
 
-					KeInitializeApc(Apc, Pethread, 0, (PKKERNEL_ROUTINE)APCKernelRoutine, 0, (PKNORMAL_ROUTINE)Hash.pvLoadLibraryExA, UserMode, pvMemory);
-					KeInsertQueueApc(Apc, 0, 0, IO_NO_INCREMENT);
-					return STATUS_SUCCESS;
-				}
+				KeInitializeApc(Apc, Pethread, 0, (PKKERNEL_ROUTINE)APCKernelRoutine, 0, (PKNORMAL_ROUTINE)Hash.pvLoadLibraryExA, UserMode, pvMemory);
+				KeInsertQueueApc(Apc, 0, 0, IO_NO_INCREMENT);
+				return STATUS_SUCCESS;
+			}
 		}
 		ZwClose(hProcess);
 	}
@@ -43,19 +43,9 @@ NTSTATUS DllInject(HANDLE ProcessId, PEPROCESS Peprocess, PETHREAD Pethread, BOO
 }
 
 
-
-PVOID
-NTAPI
-RtlxFindExportedRoutineByName(
-	_In_ PVOID DllBase,
-	_In_ PANSI_STRING ExportName
-)
+PVOID NTAPI RtlxFindExportedRoutineByName(_In_ PVOID DllBase, _In_ PANSI_STRING ExportName)
 {
-	//
-	// RtlFindExportedRoutineByName is not exported by ntoskrnl until Win10.
-	// Following code is borrowed from ReactOS.
-	//
-
+	//直到Windows 10操作系统，RtlFindExportedRoutineByName 才被ntoskrnl导出，以下代码来自React OS
 	PULONG NameTable;
 	PUSHORT OrdinalTable;
 	PIMAGE_EXPORT_DIRECTORY ExportDirectory;
@@ -65,79 +55,50 @@ RtlxFindExportedRoutineByName(
 	ULONG ExportSize;
 	PULONG ExportTable;
 
-	//
-	// Get the export directory.
-	//
-
-	ExportDirectory = RtlImageDirectoryEntryToData(DllBase,
-		TRUE,
-		IMAGE_DIRECTORY_ENTRY_EXPORT,
-		&ExportSize);
+	// 获取导入表：
+	ExportDirectory = RtlImageDirectoryEntryToData(DllBase, TRUE, IMAGE_DIRECTORY_ENTRY_EXPORT, &ExportSize);
 
 	if (!ExportDirectory)
 	{
 		return NULL;
 	}
-
-	//
 	// Setup name tables.
-	//
-
 	NameTable = (PULONG)((ULONG_PTR)DllBase + ExportDirectory->AddressOfNames);
 	OrdinalTable = (PUSHORT)((ULONG_PTR)DllBase + ExportDirectory->AddressOfNameOrdinals);
 
-	//
 	// Do a binary search.
-	//
-
 	High = ExportDirectory->NumberOfNames - 1; //导出函数
-	for (Low = 0; Low <= High;Low++) {
+	for (Low = 0; Low <= High; Low++) {
 
 		Ret = strcmp(ExportName->Buffer, (PCHAR)DllBase + NameTable[Low]);
 		if (Ret == 0) break;
 
 	}
 
-	//
 	// Check if we couldn't find it.
-	//
-
 	if (High < Low)
 	{
 		return NULL;
 	}
 
-	//
 	// Otherwise, this is the ordinal.
-	//
-
 	Ordinal = OrdinalTable[Low]; //获得导出序号
 
-	//
 	// Validate the ordinal.
-	//
-
 	if (Ordinal >= ExportDirectory->NumberOfFunctions)
 	{
 		return NULL;
 	}
 
-	//
 	// Resolve the address and write it.
-	//
-
 	ExportTable = (PULONG)((ULONG_PTR)DllBase + ExportDirectory->AddressOfFunctions);
 	Function = (PVOID)((ULONG_PTR)DllBase + ExportTable[Ordinal]);
 
-	//
 	// We found it!
-	//
-
 	NT_ASSERT(
 		(Function < (PVOID)ExportDirectory) ||
 		(Function > (PVOID)((ULONG_PTR)ExportDirectory + ExportSize))
 	);
-
 	return Function;
 }
 
@@ -175,7 +136,7 @@ VOID NTAPI APCInjectorRoutine(_In_ PKAPC Apc,
 
 	ExFreePool(Apc);
 
-	SIRIFEF_INJECTION_DATA sf; 
+	SIRIFEF_INJECTION_DATA sf;
 	RtlSecureZeroMemory(&sf, sizeof(SIRIFEF_INJECTION_DATA));
 
 	sf.Ethread = KeGetCurrentThread();
@@ -187,39 +148,33 @@ VOID NTAPI APCInjectorRoutine(_In_ PKAPC Apc,
 	ExInitializeWorkItem(&sf.workItem, (PWORKER_THREAD_ROUTINE)SirifeWorkerRoutine, &sf);
 	ExQueueWorkItem(&sf.workItem, DelayedWorkQueue);
 
-	KeWaitForSingleObject(&sf.Event, Executive,KernelMode,TRUE, 0);
+	KeWaitForSingleObject(&sf.Event, Executive, KernelMode, TRUE, 0);
 	return;
 }
 
-void LoadImageNotifyRoutine(
-	PUNICODE_STRING FullImageName,
-	HANDLE ProcessId,
-	PIMAGE_INFO ImageInfo
-) {
-
-	if (FullImageName != NULL) {
-
-		//DbgPrint("load dll: [%wZ]",FullImageName);
-
-		WCHAR kernal32Mask[] = L"*\\KERNEL32.DLL";
-		UNICODE_STRING kernel32us; 
+void LoadImageNotifyRoutine(PUNICODE_STRING FullImageName, HANDLE ProcessId, PIMAGE_INFO ImageInfo)
+{
+	if (FullImageName != NULL)
+	{
+		DbgPrint("load dll: [%wZ]", FullImageName);
+		WCHAR kernal32Mask[] = L"*\\KERNEL32.DLL"; //C:\Windows\System32\Kernel32.dll 
+		UNICODE_STRING kernel32us;
 		RtlInitUnicodeString(&kernel32us, kernal32Mask);
 
-		ANSI_STRING loadLibraryAs; 
+		ANSI_STRING loadLibraryAs;
 		RtlInitAnsiString(&loadLibraryAs, "LoadLibraryA");
 
 		if (FsRtlIsNameInExpression(&kernel32us, FullImageName, TRUE, NULL)) {
 
 			DbgPrint("Process %d Find kernel32 .", ProcessId);
 
-			PKAPC Apc; 
+			PKAPC Apc;
 			if (Hash.Kernel32dll == 0) {
 				Hash.Kernel32dll = (PVOID)ImageInfo->ImageBase;
 				// find loadlibrary function address 
 				Hash.pvLoadLibraryExA = RtlxFindExportedRoutineByName(Hash.Kernel32dll, &loadLibraryAs);
 			}
 
-		
 			Apc = (PKAPC)ExAllocatePool(NonPagedPool, sizeof(KAPC));
 
 			if (Apc) {
@@ -229,7 +184,7 @@ void LoadImageNotifyRoutine(
 			}
 		}
 	}
-	return; 
+	return;
 }
 
 
@@ -241,9 +196,7 @@ VOID Unload(IN PDRIVER_OBJECT pDriverobject) {
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 {
 	NTSTATUS status = STATUS_SUCCESS;
-
 	PsSetLoadImageNotifyRoutine(&LoadImageNotifyRoutine);
-
 	pDriverObject->DriverUnload = (PDRIVER_UNLOAD)Unload;
 	return status;
 }
